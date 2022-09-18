f93133afdbfe48f7d4336340d170ebb19725ca3d
Gaurav Agrawal <gauravag.1005@gmail.com>
2022-09-18 17:22:18 +0530

    Add project files.

---
+++ SunriseSunset/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SunriseSunset;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SunriseSunset/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SunriseSunset;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
